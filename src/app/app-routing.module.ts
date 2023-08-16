7a42c961ff5231611615629e6e986563b4b6bd13
Argueta <argueta03@hotmail.com>
2023-08-16 16:02:06 +0200

    Directivas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prodcuts',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path:'**',
+    redirectTo: 'prodcuts'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
